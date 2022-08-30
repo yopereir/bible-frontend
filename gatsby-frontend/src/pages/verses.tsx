a05812ea82614bfcff1337d5d3739bab38ef912c
ypereira <ypereira@bgea.org>
2022-08-30 11:04:33 -0400

    added homepage icon

--- gatsby-frontend/src/pages/verses.tsx
+++ gatsby-frontend/src/pages/verses.tsx
@@ -31,7 +31,6 @@ type AvatarStaticQuery = {
 
 const VersesPage = ({ title, areas, description = ``, date }: VersesPageProps) => {
   const params = new URLSearchParams(location.search);
-  const { name } = useEmiliaConfig()
   const [colorMode, setColorMode] = useColorMode()
   const isDark = colorMode === `dark`
   const [bibles, books] = [ImportFunctions.allBibles.map(bible=>bible.name),ImportFunctions.allBibleBooks.map(book=>book.Name)];
@@ -127,7 +126,7 @@ const VersesPage = ({ title, areas, description = ``, date }: VersesPageProps) =
         <animated.div style={backButtonProps}>
           <Link
             to="/"
-            aria-label={`${name} - Back to homepage`}
+            aria-label={`Back to homepage`}
             sx={{
               display: `flex`,
               alignItems: `center`,
@@ -152,11 +151,9 @@ const VersesPage = ({ title, areas, description = ``, date }: VersesPageProps) =
                 mx: 2,
               }}
             >
-              {avatar?.file?.childImageSharp?.gatsbyImageData && (
-                <GatsbyImage image={avatar.file.childImageSharp.gatsbyImageData} alt="Avatar" />
-              )}
+              <svg fill={isDark?"#fff":"#000"} xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="90%" height="90%"><path d="M 12 2.0996094 L 1 12 L 4 12 L 4 21 L 11 21 L 11 15 L 13 15 L 13 21 L 20 21 L 20 12 L 23 12 L 12 2.0996094 z M 12 4.7910156 L 18 10.191406 L 18 11 L 18 19 L 15 19 L 15 13 L 9 13 L 9 19 L 6 19 L 6 10.191406 L 12 4.7910156 z"/></svg>
             </div>
-            <span sx={{ fontWeight: `medium` }}>{name}</span>
+            <span sx={{ fontWeight: `medium` }}>Homepage</span>
           </Link>
         </animated.div>
         <div sx={{textAlign: "center", mt: 4, mb: [6, 6, 7] }}>
